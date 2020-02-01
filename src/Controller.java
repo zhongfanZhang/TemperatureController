d51c62deb0353578d77f4b3117edb7780d90552d
Zhongfan Zhang <zzf960109@gmail.com>
2020-02-01 15:10:40 +1030

    Implemented output function, changed implementation of handling invalid inputs

--- src/Controller.java
+++ src/Controller.java
@@ -5,7 +5,11 @@ public class Controller {
     //-------------------------CLASS VARIABLES---------------------------------------
     /** indices 0 and 1 are used for the temperature setpoint and deadband values respectively,
      * indices 2 to 9 are used for input temperatures */
-    private double inputs[];
+    private double[] inputs;
+    /** */
+    private double[] outputs;
+    /** */
+    private int[] outputIndex;
     /** Used for the cooling output, initially set to false by the constructor function.
      * Set cooling = true if the desired output value is ON
      * Set cooling = false if OFF */
@@ -27,11 +31,15 @@ public class Controller {
 //        deadband = 2.0;
         cooling = false;
 
-        //setup input temperatures, initialise values to 0
+        //setup initial inputs and outputs values
         inputs = new double[10];
         inputs[0] = 22.0;
         inputs[1] = 2.0;
         Arrays.fill(inputs,2,9,0);
+        outputs = new double[3];
+        Arrays.fill(outputs,0);
+        outputIndex = new int[3];
+        Arrays.fill(outputIndex,0);
     }
 
     public void connect(Display disp){
@@ -40,11 +48,42 @@ public class Controller {
 
     /** */
     public void Update(){
-        //this function will be run at regular intervals by the main function
+        //fill the output array with the three highest values
+        fillOutputArray();
 
-
+        //check if the values are valid for output, i.e. above the deadband threshold
+        for( int i = 0; i < outputs.length; i++ ){
+            //if temp is greater than the threshold, display
+            if( outputs[i] > (inputs[0] - inputs[1]) ){
+                display.displayOutputs("Input " + outputIndex[i] + ":", outputs[i], i);
+            }else{
+                display.turnOffOutput(i);
+            }
+        }
     }
 
+    /** */
+    private void fillOutputArray(){
+        double[] temp = new double[8];
+        for( int i = 0; i < 8; i++ ){
+            temp[i] = inputs[i+2];
+        }
+        for(int i = 0; i < 3; i++ ){
+            //only output if above deadband threshold
+            double max = inputs[0] - inputs[1];
+            int maxIndex = 0;
+            for( int j = 0; j < temp.length; j++ ){
+                if(temp[j] > max){
+                    max = temp[j];
+                    maxIndex = j;
+                }
+            }
+            //set the previous max to an improbable value so it wont selected for max again
+            temp[maxIndex] = -100000;
+            outputs[i] = max;
+            outputIndex[i] = maxIndex;
+        }
+    }
 
     //-------------------------------GET/SET FUNCTIONS---------------------------------
     /** Sets a specified input to a new temperature.
--- src/Display.java
+++ src/Display.java
@@ -11,6 +11,10 @@ public class Display {
     /** Index 0 and 1 and temperatureSetpoint and deadband fields respectively
      *  indices 2 to 9 are temperature inputs */
     private JTextField[] inputs;
+    /** */
+    private JTextField[] outputs;
+    /** */
+    private JLabel[] outputLabels;
 
     /** */
     public Display(){
@@ -20,12 +24,46 @@ public class Display {
         window.setSize(1920,1080);
         window.setLayout(null);
 
-        //initialising the textfields
+        //initialising the input textfields
         inputs = new JTextField[10];
-        for(int i = 0; i < 10; i++ ){
+
+        //temperature setpoint textfield
+        JLabel tempSetpointLabel = new JLabel("Temperature Setpoint:");
+        tempSetpointLabel.setBounds(100,80,150,20);
+        window.add(tempSetpointLabel);
+        inputs[0] = new JTextField();
+        inputs[0].setBounds(100,100,150,20);
+        window.add(inputs[0]);
+
+        //deadband textfield
+        JLabel deadbandLabel = new JLabel("Deadband:");
+        deadbandLabel.setBounds(300,80,150,20);
+        window.add(deadbandLabel);
+        inputs[1] = new JTextField();
+        inputs[1].setBounds(300,100,150,20);
+        window.add(inputs[1]);
+
+        //temperature inputs textfields
+        for(int i = 2; i < 10; i++ ){
+            JLabel tempInputLabel = new JLabel("Temperature input " + (i-1) + ":");
+            tempInputLabel.setBounds(100,180 + i*40,150,20);
+            window.add(tempInputLabel);
             inputs[i] = new JTextField();
-            inputs[i].setBounds(100,100 + i*20,100,20);
+            inputs[i].setBounds(100,200 + i*40,150,20);
             window.add(inputs[i]);
+        }
+
+        //initialising the output textfields
+        outputs = new JTextField[3];
+        outputLabels = new JLabel[3];
+        for(int i = 0; i < 3; i++ ){
+            outputLabels[i] = new JLabel("Output off");
+            outputLabels[i].setBounds(800,80 + i*40,150,20);
+            window.add(outputLabels[i]);
+            outputs[i] = new JTextField();
+            outputs[i].setBounds(800,100 + i*40,150,20);
+            outputs[i].setEditable(false);
+            window.add(outputs[i]);
         }
 
         window.setVisible(true);
@@ -38,27 +76,20 @@ public class Display {
         }
     }
 
+    /** */
     public void initInputs(int index){
         inputs[index].addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String text = inputs[index].getText();
-                if (checkForCharacters(text)) {
-                    System.out.println(inputs[index].getText());
-                }else{
-                    inputs[index].setText(String.valueOf(controller.getInput(index)));
+                //handle inputs other than numbers
+                try{
+                    controller.setInput(Double.parseDouble(text),index);
+                    controller.Update();
+                }catch(NumberFormatException f){
+                    JOptionPane.showMessageDialog(null,
+                            "Error: Please enter a valid numeric value.");
                 }
-            }
-            public boolean checkForCharacters(String text){
-                char ctext[] = text.toCharArray();
-                for( int i = 0; i < text.length(); i++ ){
-                    if(Character.isDigit(ctext[i]) != true && ctext[i] != '.'){
-                        JOptionPane.showMessageDialog(null,
-                                "Error: Please enter a valid numeric value.");
-                        return false;
-                    }
-                }
-                return true;
             }
         });
     }
@@ -68,7 +99,19 @@ public class Display {
         controller = cont;
     }
 
+    /** */
     public void displayTemperatureInputs(int index, double value){
         inputs[index].setText(String.valueOf(value));
+    }
+
+    /** */
+    public void displayOutputs(String label, double value, int index){
+        outputLabels[index].setText(label);
+        outputs[index].setText(String.valueOf(value));
+    }
+
+    public void turnOffOutput(int index){
+        outputLabels[index].setText("Output off");
+        outputs[index].setText(null);
     }
 }
